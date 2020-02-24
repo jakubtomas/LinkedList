380951b99cd2a40b094190bcf65158c6d19e76b6
Jakub Tomas <tomasjakub21@gmail.com>
2020-02-24 08:25:52 +0100

    first commit create 2 class

---
+++ src/sample/MyLinkedList.java
@@ -0,0 +1,38 @@
+package sample;
+
+public class MyLinkedList {
+
+    private Node head;
+
+    public MyLinkedList(Node head) {
+        this.head = head;
+    }
+
+
+    public void addToFront() {
+
+    }
+    public void addToEnd() {
+
+    }
+    public void add(int position) {
+
+    }
+
+    public void print() {
+
+    }
+
+    public void remove(Node node) {
+
+    }
+
+    public void removeAll() {
+
+    }
+
+    public void findByName(String name ) {
+
+    }
+
+}
---
+++ src/sample/Node.java
@@ -0,0 +1,30 @@
+package sample;
+
+public class Node {
+
+    private String name;
+    private Node next;
+
+
+    // set
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public Node(String name, Node next) {
+        this.name = name;
+        this.next = null;
+    }
+
+
+
+
+}
